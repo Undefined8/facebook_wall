454a49ccf28e3524a1e2118e45955cddf0612a20
undefined8 <dzamilatskiy2019@bklawtech.com>
2016-03-31 20:32:02 +0000

    i did it!

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,5 +2,13 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
-	// YOUR CODE GOES HERE
+	$("#post-btn").click(function(){
+		if($("#message").val() != ""){
+			$("#msg").html("<p>"+ $("#message").val() +"</p>");
+			var cpy = $("#tmplet").clone();
+			cpy.css("display", "block");
+			cpy.prependTo("#wall");
+			$("#message").val("");
+		}
+	});
 });
